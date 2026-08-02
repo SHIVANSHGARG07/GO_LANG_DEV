1ae759ca9c172172ee8e6c22002a2a4727cb27db
Shivansh Garg <sgarg@DDNINR0873M.local>
2026-08-03 00:21:14 +0530

    DESC: package and file structure

---
+++ channel-hello-world/basic-learn/main.go
@@ -0,0 +1,14 @@
+package main
+
+import (
+
+	// first learn is coming from go.mod whih we have init suring go mod int
+	"firstlearn/myutil"
+	"fmt"
+)
+
+func main() {
+	fmt.Println("Learning go for first time")
+
+	myutil.PrintMessage("Second learn from World")
+}
