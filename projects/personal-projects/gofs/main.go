51b3dbc08488ad469e690f27f09f54963ebc9a27
Shivansh Garg <shivanshgarg587@gmail.com>
2026-08-31 07:15:30 +0530

    feat: Support of file Content Append
    - echo
    - printf
    
    Doesnt Support:
    - touch
    
    Components added:
    1) Write()
    2) SetAttr()

--- projects/personal-projects/gofs/main.go
+++ projects/personal-projects/gofs/main.go
@@ -123,6 +123,9 @@ func (f *FileNode) Getattr(ctx context.Context, fh fs.FileHandle, out *fuse.Attr
 	// Set file mode (permissions)
 	out.Mode = fuse.S_IFREG | 0644 // Regular file, rw-r--r--
 
+	out.Uid = uint32(syscall.Getuid())
+	out.Gid = uint32(syscall.Getgid())
+
 	log.Printf("   File size: %d bytes", out.Size)
 	return 0
 }
@@ -144,4 +147,80 @@ func (f *FileNode) Read(ctx context.Context, fh fs.FileHandle, dest []byte, off 
 
 	return fuse.ReadResultData(data[off:end]), 0
 
+}
+
+/**
+Write File
+
+1) params
+content: what to write (in bytes)
+off: the position where to write, beacuse write is not for whole file, the os may split writing into several requests
+
+2) Return ??
+After storing the bytes, Write() responds how many bytes it accepted
+
+macos tells i succesfully stored all 5 bytes
+
+3) > -> means replace
+>> -> means append
+
+4) Rootnode and FileNode during Write()
+
+RootNode Jobs: "Which file does the user mean?"
+
+FileNode Jobs:"Where should these bytes go?"
+"How should the content change?"
+
+5) After writing the file size may change
+
+GetAttr must be updated , ls -l must see new file size
+**/
+
+func (f *FileNode) Write(ctx context.Context, fh fs.FileHandle, data []byte, off int64) (uint32, syscall.Errno) {
+
+	if off < 0 {
+		return 0, syscall.EINVAL
+	}
+
+	current := []byte(f.content)
+	end := int(off) + len(data)
+
+	// handle case where write extends the file
+	// create more empty spaces using bytes if needed
+	// it only creates extra spaces
+	if end > len(current) {
+		current = append(current, make([]byte, end-len(current))...)
+	}
+	copy(current[int(off):end], data)
+	f.content = string(current)
+
+	return uint32(len(data)), 0
+
+}
+
+func (f *FileNode) Setattr(
+	ctx context.Context,
+	fh fs.FileHandle,
+	in *fuse.SetAttrIn,
+	out *fuse.AttrOut,
+) syscall.Errno {
+
+	size, ok := in.GetSize()
+	if !ok {
+		return syscall.ENOTSUP
+	}
+
+	current := []byte(f.content)
+
+	if size < uint64(len(current)) {
+		current = current[:size]
+	} else if size > uint64(len(current)) {
+		current = append(current,
+			make([]byte, int(size)-len(current))...)
+	}
+
+	f.content = string(current)
+
+	// Return the updated size and other attributes
+	return f.Getattr(ctx, fh, out)
 }
